cf46eeda404e3edc7b0fb3492941b28cc369cf07
João Lourenço <jlourenco5691@gmail.com>
2020-10-30 21:54:29 +0000

    docker: Add Docker
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- frontend/source/app.d
+++ frontend/source/app.d
@@ -4,7 +4,7 @@ void main()
 {
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.bindAddresses = ["::"];
 	listenHTTP(settings, &hello);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
