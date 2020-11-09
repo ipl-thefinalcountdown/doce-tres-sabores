5f310ac8d0768bff3e42288b66178e44a2034529
Luís Ferreira <contact@lsferreira.net>
2020-11-09 21:55:45 +0000

    ws: implement webservice api endpoints
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ src/main/java/io/github/ipl/tfc/docetressabores/ws/ProductService.java
@@ -0,0 +1,48 @@
+package io.github.ipl.tfc.docetressabores.ws;
+
+import io.github.ipl.tfc.docetressabores.entities.Product;
+import io.github.ipl.tfc.docetressabores.dtos.ProductDTO;
+import io.github.ipl.tfc.docetressabores.dtos.VariantDTO;
+import io.github.ipl.tfc.docetressabores.ejbs.ProductBean;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/products")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
+public class ProductService
+{
+	@EJB
+	private ProductBean productBean;
+
+	public static ProductDTO toDTO(Product product)
+	{
+		List<VariantDTO> variantDTOs = VariantService.toDTOs(product.getVariants());
+		return new ProductDTO(product.getName(), variantDTOs);
+	}
+
+	public static List<ProductDTO> toDTOs(List<Product> products)
+	{
+		return products
+			.stream()
+			.map(ProductService::toDTO)
+			.collect(Collectors.toList());
+	}
+
+	@GET
+	@Path("/all")
+	public List<ProductDTO> getAllProductsWS()
+	{
+		return toDTOs(productBean.getAllProducts());
+	}
+}
