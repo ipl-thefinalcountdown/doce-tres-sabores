c65e2ed71fadc184fe3493363dab436ce53e2981
João Lourenço <jlourenco5691@gmail.com>
2020-11-12 23:37:36 +0000

    project: Add route paths to manipulate Project
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- src/main/java/io/github/ipl/tfc/docetressabores/ejbs/ProjectBean.java
+++ src/main/java/io/github/ipl/tfc/docetressabores/ejbs/ProjectBean.java
@@ -1,14 +1,18 @@
 package io.github.ipl.tfc.docetressabores.ejbs;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
 
 import io.github.ipl.tfc.docetressabores.dtos.ProjectDTO;
 import io.github.ipl.tfc.docetressabores.entities.Client;
 import io.github.ipl.tfc.docetressabores.entities.Project;
+import io.github.ipl.tfc.docetressabores.entities.Structure;
 
 @Stateless
 public class ProjectBean {
@@ -30,6 +34,25 @@ public class ProjectBean {
 		}
 	}
 
+	public Project update(ProjectDTO projectDTO) {
+		Project project = findProject(projectDTO.getId());
+
+		project.setName(projectDTO.getName());
+		project.setClient(entityManager.find(Client.class, projectDTO.getClientId()));
+		project.setStructures(
+			projectDTO.getStructures()
+				.stream()
+				.map(s -> entityManager.find(Structure.class, s.getId()))
+				.collect(Collectors.toList())
+		);
+
+		return project;
+	}
+
+	public void delete(int id) {
+		entityManager.remove(findProject(id));
+	}
+
 	public void create(ProjectDTO projectDTO) {
 		create(projectDTO.getName(), projectDTO.getClientId());
 	}
@@ -38,9 +61,10 @@ public class ProjectBean {
 		return entityManager.find(Project.class, id);
 	}
 
-	public List<Project> getAllProjects() {
+	public List<Project> getAllProjects(@NotNull String name) {
 		return entityManager
 			.createNamedQuery("getAllProjects", Project.class)
+			.setParameter("name", name)
 			.getResultList();
 	}
 }
--- src/main/java/io/github/ipl/tfc/docetressabores/ws/ProjectService.java
+++ src/main/java/io/github/ipl/tfc/docetressabores/ws/ProjectService.java
@@ -7,9 +7,15 @@ import java.util.stream.Collectors;
 import javax.ejb.EJB;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import io.github.ipl.tfc.docetressabores.dtos.ProjectDTO;
@@ -22,25 +28,61 @@ import io.github.ipl.tfc.docetressabores.entities.Project;
 public class ProjectService {
 	@EJB ProjectBean projectBean;
 
-	public static ProjectDTO toDTO(Project project) {
+	public static ProjectDTO toDTO(Project project, boolean critical) {
 		return new ProjectDTO(
 			project.getId(),
 			project.getName(),
 			project.getClient().getId(),
-			StructureService.toDTOs(project.getStructures()));
+			critical ? null : StructureService.toDTOs(project.getStructures()));
+	}
+
+	public static ProjectDTO toDTO(Project project) {
+		return toDTO(project, true);
+	}
+
+	public static List<ProjectDTO> toDTOs(List<Project> projects, boolean critical) {
+		return projects
+			.stream()
+			.map(p -> toDTO(p, critical))
+			.collect(Collectors.toList());
 	}
 
 	public static List<ProjectDTO> toDTOs(List<Project> projects) {
-		return projects
-			.stream()
-			.map(ProjectService::toDTO)
-			.collect(Collectors.toList());
+		return toDTOs(projects, true);
 	}
 
 	@GET
 	@Path("/all")
 	@Transactional
-	public List<ProjectDTO> getAllProjectsWS() {
-		return toDTOs(projectBean.getAllProjects());
+	public List<ProjectDTO> getAllProjectsWS(@DefaultValue("") @QueryParam("name") String name) {
+		return toDTOs(projectBean.getAllProjects(name));
+	}
+
+	@GET
+	@Path("/{id}")
+	@Transactional
+	public ProjectDTO getProjectWS(@PathParam("id") int id) {
+		return toDTO(projectBean.findProject(id));
+	}
+
+	@POST
+	@Path("/")
+	@Transactional
+	public void postProjectWS(ProjectDTO projectDTO) {
+		projectBean.create(projectDTO);
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Transactional
+	public void updateProjectWS(@PathParam("id") int id, ProjectDTO projectDTO) {
+		projectBean.update(projectDTO);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Transactional
+	public void deleteProjectWS(@PathParam("id") int id) {
+		projectBean.delete(id);
 	}
 }
