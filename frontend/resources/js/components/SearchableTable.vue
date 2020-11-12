7336b88f9ec21d16cf534c7b073c356da8fdef7b
Luís Ferreira <contact@lsferreira.net>
2020-11-12 16:59:33 +0000

    frontend: add support for webpack and add simple vue app
    - add webpack support to bundle bootstrap and fontawesome
    - prepare simple mvc structure to add vue app
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ frontend/resources/js/components/SearchableTable.vue
@@ -0,0 +1,20 @@
+<template>
+	<b-table striped hover :items="items" :fields="fields"></b-table>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component
+export default class VariantsView extends Vue {
+	props: ['fields', 'items']
+
+	// data() {
+    //   return {
+    //     fields,
+    //     items
+    //   }
+    // }
+}
+</script>
