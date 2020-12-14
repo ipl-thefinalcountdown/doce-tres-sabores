2b9c36aa965ca883fdbf380052920df93cc74280
Luís Ferreira <contact@lsferreira.net>
2020-12-14 15:29:56 +0000

    frontend: add routes to the vue-router
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- frontend/resources/js/router.ts
+++ frontend/resources/js/router.ts
@@ -7,9 +7,11 @@ import IndexView from "./views/index.vue"
 import LoginView from "./views/login.vue"
 import VariantListView from './views/variants/list.vue'
 import VariantView from './views/variants/view.vue'
+import VariantAddEditView from './views/variants/addEdit.vue'
 import ProjectListView from './views/projects/list.vue'
 import ProjectView from './views/projects/view.vue'
 import ProjectAddEditView from './views/projects/addEdit.vue'
+// import StructureListView from './views/structures/list.vue'
 
 Vue.use(VueRouter)
 
@@ -18,12 +20,35 @@ export default new VueRouter({
 
 	routes: [
 		{ path: '/', name: 'index', component: IndexView },
-		{ path: '/login', name: 'login', component: LoginView },
+
 		{ path: '/variants', name: 'list-variants', component: VariantListView },
+		{ path: '/variants/new', name: 'new-variant', component: VariantAddEditView },
 		{ path: '/variants/:id', name: 'view-variant', component: VariantView },
+		{ path: '/variants/:id/edit', name: 'edit-variant', component: VariantAddEditView },
+
 		{ path: '/projects', name: 'list-projects', component: ProjectListView },
 		{ path: '/projects/new', name: 'new-project', component: ProjectAddEditView },
 		{ path: '/projects/:id', name: 'view-project', component: ProjectView },
-		{ path: '/projects/:id/edit', name: 'edit-project', component: ProjectAddEditView }
+		{ path: '/projects/:id/edit', name: 'edit-project', component: ProjectAddEditView },
+
+		{ path: '/login', name: 'login', component: LoginView },
+		// { path: '/register', name: 'login', component: RegisterView },
+
+		// { path: '/users/:id', name: 'view-user', component: UserView },
+		// { path: '/users/:id/edit', name: 'edit-user', component: UserEditView },
+
+		// { path: '/structures', name: 'list-structures', component: StructureListView },
+		// { path: '/structures/new', name: 'new-structure', component: StructureAddEditView },
+		// { path: '/structures/:id', name: 'view-project', component: StructureView },
+		// { path: '/structures/:id/edit', name: 'edit-project', component: StructureAddEditView },
+
+		// { path: '/simulation', name: 'view-project', component: SimulationIndexView },
+
+		// { path: '/structures/simulation', name: 'view-project', component: SimulationView },
+		// { path: '/structures/:id/simulation', name: 'view-project', component: StructureSimulationView },
+
+		// { path: '/products', name: 'list-products', component: ProductListView },
+		// { path: '/products/new', name: 'new-product', component: ProductAddEditView },
+		// { path: '/products/:id', name: 'view-product', component: ProductView },
 	]
 })
