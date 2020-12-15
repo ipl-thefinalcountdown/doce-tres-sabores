9f187f5327649e438b6730dc883529c7d00657a0
João Lourenço <jlourenco5691@gmail.com>
2020-12-15 17:19:17 +0000

    manufacturer: add manufacturer user and respective GET requests
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ src/main/java/io/github/ipl/tfc/docetressabores/ws/ManufacturerService.java
@@ -0,0 +1,62 @@
+package io.github.ipl.tfc.docetressabores.ws;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ejb.EJB;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.github.ipl.tfc.docetressabores.dtos.UserDTO;
+import io.github.ipl.tfc.docetressabores.ejbs.ManufacturerBean;
+import io.github.ipl.tfc.docetressabores.entities.Manufacturer;
+
+@Path("/manufacturers")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
+public class ManufacturerService {
+	@EJB ManufacturerBean manufacturerBean;
+
+	public static UserDTO toDTO(Manufacturer manufacturer) {
+		return new UserDTO(
+			manufacturer.getId(),
+			manufacturer.getName(),
+			manufacturer.getPhoneNumber(),
+			manufacturer.getEmail(),
+			manufacturer.getUsername(),
+			manufacturer.getPassword(),
+			null,
+			null,
+			manufacturer.getClass().getSimpleName()
+		);
+	}
+
+	public static List<UserDTO> toDTOs(List<Manufacturer> manufacturer) {
+		return manufacturer
+			.stream()
+			.map(ManufacturerService::toDTO)
+			.collect(Collectors.toList());
+	}
+
+	@GET
+	@Path("/")
+	@Transactional
+	public Response getAllManufacturersWS() {
+		return Response.ok(toDTOs(manufacturerBean.getAllManufacturers())).build();
+	}
+
+	@GET
+	@Path("/{id}")
+	@Transactional
+	public Response getClientWS(@PathParam("id") int id) {
+		Manufacturer manufacturer = manufacturerBean.findManufacturer(id);
+
+		return (
+			manufacturer == null
+				? Response.status(Response.Status.BAD_REQUEST)
+				: Response.ok(toDTO(manufacturer))
+			).build();
+	}
+}
