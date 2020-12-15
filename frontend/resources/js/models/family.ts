7186218a6d749cf26f29c481a50eae4b979036d0
Luís Ferreira <contact@lsferreira.net>
2020-12-15 20:36:51 +0000

    frontend: add models: materialId to family model
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- frontend/resources/js/models/family.ts
+++ frontend/resources/js/models/family.ts
@@ -1,7 +1,9 @@
+import { MaterialType } from './material';
 import ProductModel from './product';
 
 export default interface FamilyModel {
 	id?: number;
 	name?: string;
+	materialId?: MaterialType;
 	products?: ProductModel[];
 }
