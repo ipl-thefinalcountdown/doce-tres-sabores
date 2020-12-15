10f71857452851b303677af9be286c920b2d9746
João Lourenço <jlourenco5691@gmail.com>
2020-12-15 21:04:36 +0000

    login: add login service
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ src/main/java/io/github/ipl/tfc/docetressabores/ws/LoginService.java
@@ -0,0 +1,31 @@
+package io.github.ipl.tfc.docetressabores.ws;
+
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.github.ipl.tfc.docetressabores.ejbs.UserBean;
+import io.github.ipl.tfc.docetressabores.entities.User;
+
+@Path("/auth")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes(MediaType.APPLICATION_JSON)
+public class LoginService {
+	private static final Logger LOG = Logger.getLogger(LoginService.class.getName());
+	@EJB UserBean userBean;
+
+	@POST
+	@Path("/login")
+	@Transactional
+	public Response authenticateUserWS(@FormParam("username") String username, @FormParam("password") String password) {
+		User user = userBean.authenticate(username, password);
+
+		if (user == null) Response.status(Response.Status.UNAUTHORIZED).build();
+
+		return Response.ok().build();
+	}
+}
