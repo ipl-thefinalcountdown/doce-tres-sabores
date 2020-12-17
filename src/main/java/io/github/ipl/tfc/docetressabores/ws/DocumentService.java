211a6b0280c1f614b2fc510884ae91397251aa5e
João Lourenço <jlourenco5691@gmail.com>
2020-12-17 22:57:35 +0000

    document: add DELETE request
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- src/main/java/io/github/ipl/tfc/docetressabores/ws/DocumentService.java
+++ src/main/java/io/github/ipl/tfc/docetressabores/ws/DocumentService.java
@@ -4,11 +4,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.ejb.EJB;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.github.ipl.tfc.docetressabores.dtos.DocumentDTO;
 import io.github.ipl.tfc.docetressabores.ejbs.DocumentBean;
 import io.github.ipl.tfc.docetressabores.entities.Document;
 
+@Path("/documents")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
 public class DocumentService {
 	@EJB DocumentBean documentBean;
 
@@ -23,5 +30,17 @@ public class DocumentService {
 
 	public static List<DocumentDTO> toDTOs(List<Document> documents) {
 		return documents.stream().map(DocumentService::toDTO).collect(Collectors.toList());
+	}
+
+
+	@DELETE
+	@Path("/{id}")
+	@Transactional
+	public Response downloadDocumentWS(@PathParam("id") int id) {
+		return (
+			documentBean.deleteDocument(id)
+				? Response.noContent()
+				: Response.status(Response.Status.BAD_REQUEST)
+			).build();
 	}
 }
