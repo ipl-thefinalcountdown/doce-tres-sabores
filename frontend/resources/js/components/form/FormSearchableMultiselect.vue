c6954043b7b1d7212db49c820709909d6c4a329f
Luís Ferreira <contact@lsferreira.net>
2020-12-18 11:44:34 +0000

    frontend: components: add a searchable multiselect component
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ frontend/resources/js/components/form/FormSearchableMultiselect.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-form-group :label="label" :description="description">
+	<multi-select :options="options" :selected-options="value" @select="handleInput" :placeholder="placeholder" />
+  </b-form-group>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { MultiSelect } from 'vue-search-select';
+
+export default Vue.extend({
+	 props: {
+	  value: Array,
+      label: String,
+      description: String,
+	  placeholder: String,
+	  options: Array
+  },
+  model: {
+      prop: 'value',
+      event: 'onFieldChanged'
+  },
+  methods: {
+    handleInput(items : Array<any>, lastSelectItem: any)
+    {
+      this.$emit('onFieldChanged', items);
+    }
+  },
+  components: {
+	  MultiSelect
+  }
+});
+</script>
